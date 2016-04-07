7cb1daf57d1174a80c10ba35cfb0c74709fa5c6f
patharanordev <patharanor.dev@gmail.com>
2016-04-08 00:07:06 +0700

    Update sma.js

--- sma.js
+++ sma.js
@@ -19,3 +19,22 @@ var simpleMovingAVG = function(dataArray, timePeriods, resultArr){
 
 	return result;
 };
+
+var simpleMovingAVGWithObject = function(dataObjArray, timePeriods){
+	var sum = 0;
+	var result = false;
+
+	try{
+		for(var i=timePeriods-1;i>-1;i--){
+			sum += dataObjArray[i].data;
+		}
+
+		result = (parseFloat(sum) / parseFloat(timePeriods));
+		console.log('SMA Result : ' + result);
+	} catch(err) {
+		result = false;
+		console.log("SMA Error : " + err);
+	}
+
+	return result;
+};
